eb649fbadd385ad5fcdce27e378267587cdfbaab
Ayush <ec2-user@ip-172-31-21-227.ec2.internal>
2024-04-14 21:35:30 +0000

    Changes in the ALB and ASG

--- Module/aws_webserver/install_httpd.sh.tpl
+++ Module/aws_webserver/install_httpd.sh.tpl
@@ -7,7 +7,8 @@ echo "<h1> We are Group 1.  Private IP is $myip this is hosted in private subnet
     <li>Ayush Nepal</li>
     <li>Sandesh Maharjan</li>
 </ul> <br>
-The Images are:
-<img src="https://finalprojectacs.s3.amazonaws.com/pictures/cat.jpeg" alt="pussycat">"  >  /var/www/html/index.html
+The Images are: <br>
+<img src="https://finalprojectacs.s3.amazonaws.com/Images/ilovedogs.jpg" height="50" width="50" alt="dog"> 
+<img src="https://finalprojectacs.s3.amazonaws.com/Images/ilovecats.jpg" height="50" width="50" alt="cat">" >  /var/www/html/index.html
 sudo systemctl start httpd
 sudo systemctl enable httpd
--- Module/aws_webserver/main.tf
+++ Module/aws_webserver/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+//fetching the image for the instance
 data "aws_ami" "latest_amazon_linux" {
   owners      = ["amazon"]
   most_recent = true
@@ -11,6 +12,7 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
+//retrieving the network configuration
 data "terraform_remote_state" "network_output" {
   backend = "s3"
   config = {
@@ -20,6 +22,7 @@ data "terraform_remote_state" "network_output" {
   }
 }
 
+//creating the vm for the prod
 resource "aws_instance" "vm" {
   count                       = length(data.terraform_remote_state.network_output.outputs.private_ip)
   ami                         = data.aws_ami.latest_amazon_linux.id
@@ -37,14 +40,15 @@ resource "aws_instance" "vm" {
   tags = {
     Name = "${var.env}--VM-${count.index + 1}"
   }
-
 }
 
+//setting up the key pair
 resource "aws_key_pair" "web_key" {
   key_name   = "${var.env}-Assignment_key"
   public_key = file("finalproject.pub")
 }
 
+//creating the security group for the vm
 resource "aws_security_group" "vm_sg" {
   name        = "allow_http_ssh"
   description = "allow http ssh from bastion"
@@ -81,7 +85,7 @@ resource "aws_security_group" "vm_sg" {
   }
 }
 
-
+//creating the bastion host
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.latest_amazon_linux.id
   instance_type               = var.instance_types[var.env]
@@ -95,6 +99,7 @@ resource "aws_instance" "bastion" {
   }
 }
 
+//security group for the bastion host
 resource "aws_security_group" "bastion_sg" {
   name        = "bastion_allow_http_ssh"
   description = "allow http ssh for bastion"
@@ -131,8 +136,9 @@ resource "aws_security_group" "bastion_sg" {
   }
 }
 
-resource "aws_security_group" "web_alb_sg" {
-  name        = "web_alb_sg"
+//creating the security group for the load balancer
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_sg"
   description = "Security group for ALB"
   vpc_id      = data.terraform_remote_state.network_output.outputs.vpc_id
 
@@ -151,23 +157,25 @@ resource "aws_security_group" "web_alb_sg" {
   }
 
   tags = {
-    Name = "Web-ALB-SG"
+    Name = "ALB-SG"
   }
 }
 
-resource "aws_lb" "web_alb" {
-  name               = "web-alb"
+//creating the load balancer
+resource "aws_lb" "ALB" {
+  name               = "ALB"
   internal           = false
   load_balancer_type = "application"
   subnets            = data.terraform_remote_state.network_output.outputs.public_ip
-  security_groups    = [aws_security_group.web_alb_sg.id]
+  security_groups    = [aws_security_group.lb_sg.id]
 
   tags = {
-    Name = "Web-ALB"
+    Name = "ALB"
   }
 }
 
-resource "aws_lb_target_group" "web_target_group" {
+//creating the target group
+resource "aws_lb_target_group" "target_group" {
   name     = "web-target-group"
   port     = 80
   protocol = "HTTP"
@@ -187,33 +195,27 @@ resource "aws_lb_target_group" "web_target_group" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "vm1_attachment" {
-  count            = length(aws_instance.vm)
-  target_group_arn = aws_lb_target_group.web_target_group.arn
-  target_id        = aws_instance.vm[count.index].id
-}
+//creating the launch template as the ami of our vm
+resource "aws_launch_template" "web_launch_template" {
+  name_prefix      = "${var.env}-web-launch-template"
+  image_id         = data.aws_ami.latest_amazon_linux.id
+  instance_type    = var.instance_types[var.env]
+  key_name         = aws_key_pair.web_key.key_name
+  user_data        = base64encode(file("${path.module}/install_httpd.sh.tpl"))
 
-resource "aws_lb_listener" "web_listener" {
-  load_balancer_arn = aws_lb.web_alb.arn
-  port              = 80
-  protocol          = "HTTP"
+  block_device_mappings {
+    device_name = "/dev/sda1"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web_target_group.arn
+    ebs {
+      volume_size = 50
+      volume_type = "gp2"
+      encrypted   = true
+    }
   }
-}
 
-resource "aws_launch_configuration" "web_launch_config" {
-  name_prefix   = "${var.env}-web-launch-config"
-  image_id      = data.aws_ami.latest_amazon_linux.id
-  instance_type = var.instance_types[var.env]
-  key_name      = aws_key_pair.web_key.key_name
-  security_groups = [aws_security_group.vm_sg.id]
-  user_data     = file("${path.module}/install_httpd.sh.tpl")
-
-  root_block_device {
-    encrypted = true
+  network_interfaces {
+    security_groups = [aws_security_group.vm_sg.id]
+    associate_public_ip_address = false
   }
 
   lifecycle {
@@ -221,13 +223,19 @@ resource "aws_launch_configuration" "web_launch_config" {
   }
 }
 
-resource "aws_autoscaling_group" "web_asg" {
+//creating the auto scaling group
+resource "aws_autoscaling_group" "ASG" {
   name                 = "${var.env}-web-asg"
   vpc_zone_identifier  = data.terraform_remote_state.network_output.outputs.private_ip
-  launch_configuration = aws_launch_configuration.web_launch_config.name
+  launch_template {
+    id      = aws_launch_template.web_launch_template.id
+    version = aws_launch_template.web_launch_template.latest_version
+  }
   min_size             = 1
-  max_size             = 3
+  max_size             = 4
   desired_capacity     = 2
+
+  target_group_arns = [aws_lb_target_group.target_group.arn]
 
   tag {
     key                 = "Name"
@@ -240,3 +248,30 @@ resource "aws_autoscaling_group" "web_asg" {
   }
 }
 
+//creating the scale out policy as per cpu usage
+resource "aws_autoscaling_policy" "scale_out_policy" {
+  name                   = "${var.env}-scale-out-policy"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.ASG.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 10  
+  }
+}
+
+//creating the scale in policy as per cpu usage
+resource "aws_autoscaling_policy" "scale_in_policy" {
+  name                   = "${var.env}-scale-in-policy"
+  policy_type            = "TargetTrackingScaling"
+  autoscaling_group_name = aws_autoscaling_group.ASG.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 5  
+  }
+}
