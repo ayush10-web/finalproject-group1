9a7bd55d30536fca30c52ff73c1b1231a2347f78
sandesh <ec2-user@ip-172-31-41-126.ec2.internal>
2024-04-14 00:41:18 +0000

    Changes in the staging/webserver

--- Project/staging/webserver/variables.tf
+++ Project/staging/webserver/variables.tf
@@ -3,13 +3,3 @@ variable "env" {
   type        = string
   description = "Deployment Environment"
 }
-
-variable "instance_types" {
-  default = {
-    dev     = "t3.micro"
-    prod    = "t3.micro"
-    staging = "t3.small"
-  }
-  type        = map(string)
-  description = "Instance types for different environments"
-}
