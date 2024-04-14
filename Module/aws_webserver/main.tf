9ebbee01618c1ea6fbecb73eedb1bd92f71ce5e4
sandesh <ec2-user@ip-172-31-41-126.ec2.internal>
2024-04-14 01:49:28 +0000

    Deployment of ALB and ASG in Module/aws_webserver

--- Module/aws_webserver/main.tf
+++ Module/aws_webserver/main.tf
@@ -76,7 +76,6 @@ resource "aws_security_group" "vm_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-
   tags = {
     Name = "${var.env}-VM-Bastion_SG"
   }
@@ -127,9 +126,117 @@ resource "aws_security_group" "bastion_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-
   tags = {
     Name = "${var.env}--Bastion_SG"
+  }
+}
+
+resource "aws_security_group" "web_alb_sg" {
+  name        = "web_alb_sg"
+  description = "Security group for ALB"
+  vpc_id      = data.terraform_remote_state.network_output.outputs.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-ALB-SG"
+  }
+}
+
+resource "aws_lb" "web_alb" {
+  name               = "web-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = data.terraform_remote_state.network_output.outputs.public_ip
+  security_groups    = [aws_security_group.web_alb_sg.id]
+
+  tags = {
+    Name = "Web-ALB"
+  }
+}
+
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "web-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.network_output.outputs.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "Web-Target-Group"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "vm1_attachment" {
+  count            = length(aws_instance.vm)
+  target_group_arn = aws_lb_target_group.web_target_group.arn
+  target_id        = aws_instance.vm[count.index].id
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+}
+
+resource "aws_launch_configuration" "web_launch_config" {
+  name_prefix   = "${var.env}-web-launch-config"
+  image_id      = data.aws_ami.latest_amazon_linux.id
+  instance_type = var.instance_types[var.env]
+  key_name      = aws_key_pair.web_key.key_name
+  security_groups = [aws_security_group.vm_sg.id]
+  user_data     = file("${path.module}/install_httpd.sh.tpl")
+
+  root_block_device {
+    encrypted = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "web_asg" {
+  name                 = "${var.env}-web-asg"
+  vpc_zone_identifier  = data.terraform_remote_state.network_output.outputs.private_ip
+  launch_configuration = aws_launch_configuration.web_launch_config.name
+  min_size             = 1
+  max_size             = 3
+  desired_capacity     = 2
+
+  tag {
+    key                 = "Name"
+    value               = "${var.env}-Web-Instance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
